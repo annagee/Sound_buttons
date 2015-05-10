d15ae5616b2a51af6caac3b1a1307c542f8270f5
Anna Gaughan <agaughan@gmail.com>
2015-05-10 08:03:16 +0100

    getting hmtl and css working

---
+++ scripts/main_with_jQuery.js
@@ -0,0 +1,63 @@
+// console.log('help');
+
+ $('document').ready(function () {
+
+
+// // soundManager.onload = function(){soundManager.createSound({
+// //    id: English',
+//       url: '/audiofiles/english.mp3’);
+// //   soundManager.play('English');
+
+
+// // };
+
+
+soundManager.setup({
+ url: 'swf/',
+ onready: function() {
+
+ $("#English").on("click", function(){
+ var mySound = soundManager.createSound({
+id: 'English',
+ url: './audiofiles/english.mp3'
+ });
+ mySound.play();
+
+
+// $("#Chinese").on("click", function(){
+ // var mySound = soundManager.createSound({
+  // id: 'Chinese',
+ // url: './audiofiles/chinese.mp3'
+// });
+ // mySound.play();
+
+
+
+})
+ },
+ ontimeout: function() {
+ }
+ });
+
+
+// $('English').on('click', soundManager.onload);
+
+
+
+  
+// }
+
+
+
+//   $('.greeting').on("click", showGreeting);
+
+// function showGreeting(){
+
+//  // $('.greeting').on("click", function(){
+//   console.log("click"); 
+//  var output = "WDI 13 speaks:"  + $(this).attr('id');
+//   $('p').text(output);
+
+// }
+
+});
